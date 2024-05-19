a4b83aff535c8c295cbdc2f536473c19cc0d7826
Ilya Kondratyuk <ikondratyuk@systematica-consulting.ru>
2024-05-20 01:42:18 +0700

    Initial commit

---
+++ consumer-service/src/main/java/org/weare4saken/Main.java
@@ -0,0 +1,7 @@
+package org.weare4saken;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ producer-service/src/main/java/org/weare4saken/ProducerServiceApplication.java
@@ -0,0 +1,7 @@
+package org.weare4saken;
+
+public class ProducerServiceApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
